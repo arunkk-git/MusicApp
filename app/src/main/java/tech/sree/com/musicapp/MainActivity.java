295588b8848acd5d22a07a2e835eeaa3b4dd59a3
Arun Kulkarni <kulkarni.arun1382@gmail.com>
2016-04-12 22:28:23 +0530

    Music App Intial code.

--- app/src/main/java/tech/sree/com/musicapp/MainActivity.java
+++ app/src/main/java/tech/sree/com/musicapp/MainActivity.java
@@ -4,12 +4,18 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
@@ -20,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    private TextView setTime;
+    private TextView setTime,toastText;
     private TimePicker timePicker;
     private PendingIntent pi;
     private AlarmManager am;
@@ -32,6 +38,7 @@ Button playList;
      //   setTime = (TextView) findViewById(R.id.setTime);
         timePicker = (TimePicker)findViewById(R.id.timePicker);
         playList = (Button)findViewById(R.id.playList);
+        toastText = (TextView)findViewById(R.id.toastText);
 
     }
 
@@ -80,7 +87,19 @@ Button playList;
         }
         Calendar futureDate = Calendar.getInstance();
         // futureDate.setTimeZone(TimeZone.getTimeZone("GMT"));
-        Toast.makeText(getApplicationContext(),"Future set for [ "+hour+" : "+minute+"  ]",Toast.LENGTH_LONG).show();
+        String info =" Future set for [ "+hour+" : "+minute+"  ]";
+     //   toastText.setText(info);
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast,
+                (ViewGroup) findViewById(R.id.custom_toast_root));
+        TextView text = (TextView) layout.findViewById(R.id.toastText);
+        text.setText(info);
+        Toast toast = new Toast(getApplicationContext());
+       // toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
+                //.makeText(getApplicationContext(),"Future set for [ "+hour+" : "+minute+"  ]",Toast.LENGTH_LONG).show();
         futureDate.set(Calendar.HOUR_OF_DAY, hour);
         futureDate.set(Calendar.MINUTE, minute);
 
@@ -89,4 +108,37 @@ Button playList;
         am = (AlarmManager) getSystemService(ALARM_SERVICE);
         am.set(AlarmManager.RTC_WAKEUP,futureDate.getTimeInMillis(), pi);
     }
+  /*
+    public void customToast(String info){
+        LinearLayout layout=new LinearLayout(this);
+        layout.setBackgroundResource(R.color.LightOrange);
+
+        TextView  tv=new TextView(this);
+        // set the TextView properties like color, size etc
+        tv.setTextColor(Color.RED);
+        tv.setTextSize(15);
+
+        tv.setGravity(Gravity.CENTER_VERTICAL);
+
+        // set the text you want to show in  Toast
+        tv.setText("My Custom Toast at Bottom of Screen");
+
+        ImageView   img=new ImageView(this);
+
+        // give the drawble resource for the ImageView
+        img.setImageResource("mipmap/ic_launcher");
+
+        // add both the Views TextView and ImageView in layout
+        layout.addView(img);
+        layout.addView(tv);
+
+        Toast toast=new Toast(this); //context is object of Context write "this" if you are an Activity
+        // Set The layout as Toast View
+        toast.setView(layout);
+
+        // Position you toast here toast position is 50 dp from bottom you can give any integral value
+        toast.setGravity(Gravity.BOTTOM, 0, 50);
+        toast.show();
+    }
+    */
 }
