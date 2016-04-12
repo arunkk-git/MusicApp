9e3ca667e313493adb683b1669e7cf2bcaad12b8
Arun Kulkarni <kulkarni.arun1382@gmail.com>
2016-04-12 23:07:12 +0530

    Toast and niotification is added .

--- app/src/main/java/tech/sree/com/musicapp/MusicPlay.java
+++ app/src/main/java/tech/sree/com/musicapp/MusicPlay.java
@@ -1,5 +1,8 @@
 package tech.sree.com.musicapp;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
+import android.app.Notification;
+import android.app.NotificationManager;
 
 import tech.sree.com.musicapp.R;
 
@@ -25,6 +30,7 @@ public class MusicPlay extends AppCompatActivity {
         mPlayer = MediaPlayer.create(getApplicationContext(),R.raw.music);
 
         playSound();
+        Notify("Music Player","Plaing Song Number ~~~~~~~");
     }
     public void exitMusicApp(View V){
         mPlayer.stop();
@@ -36,5 +42,32 @@ public class MusicPlay extends AppCompatActivity {
         mPlayer.start();
 
     }
+
+
+    private void Notify(String notificationTitle, String notificationMessage){
+// prepare intent which is triggered if the
+// notification is selected
+
+        Intent intent = new Intent(this, MusicPlay.class);
+// use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+// build notification
+// the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle(notificationTitle)
+                .setContentText(notificationMessage)
+                .setSmallIcon(R.drawable.music)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.music, "play", pIntent)
+                .addAction(R.drawable.music, "pause", pIntent)
+                .addAction(R.drawable.music, "Exit", pIntent).build();
+
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, n);   }
 
 }
